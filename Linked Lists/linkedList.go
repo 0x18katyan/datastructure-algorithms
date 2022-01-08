f3605f14b67f54badd4460a1b5f53a05157c0ce7
Ashim Mahara <katyansun@gmail.com>
2022-01-08 18:13:40 +0545

    Update working

--- Linked Lists/linkedList.go
+++ Linked Lists/linkedList.go
@@ -36,13 +36,22 @@ func (ll *linkedList) append(value int) {
 }
 
 // Will have to go to that position and shift all others to the next?
-func (ll *linkedList) update(position int) {
+func (ll *linkedList) update(position int, value int) error {
 
+	node, err := ll.getNode(position)
+
+	if err != nil {
+		return fmt.Errorf("Error getting value at %v : %v .\n", ll.elem, err)
+	}
+
+	*node = link{value: value, next: node.next}
+
+	return err
 }
 
 // Iterates over the linkedList to get the value at link; starts at 1
-func (ll *linkedList) get(position int) (link, error) {
-	var link link
+func (ll *linkedList) getNode(position int) (*link, error) {
+	var link *link
 
 	if position == 0 {
 
@@ -50,10 +59,10 @@ func (ll *linkedList) get(position int) (link, error) {
 
 	} else if position == 1 {
 
-		return ll.head, nil
+		return &ll.head, nil
 
 	} else if position == ll.elem || position == -1 {
-		return ll.tail, nil
+		return &ll.tail, nil
 
 	} else if position > ll.elem {
 
@@ -61,20 +70,17 @@ func (ll *linkedList) get(position int) (link, error) {
 
 	} else {
 
-		link = *ll.head.next
+		link = ll.head.next
 		for i := 2; i <= position; i++ {
 			if i == position {
 				break
 			} else {
-				link = *link.next
+				link = link.next
 			}
 
 		}
-
 	}
-
 	return link, nil
-
 }
 
 func main() {
@@ -88,10 +94,16 @@ func main() {
 	ll.append(7)
 	ll.append(8)
 
-	fmt.Println("Linked List is", ll)
+	node := 4
+	ll.update(4, 12)
 
-	node := 6
-	link, err := ll.get(node)
+	link, err := ll.getNode(node)
+
+	fmt.Println("Linked List is", ll)
+	fmt.Println("ll.head", ll.head)
+	fmt.Println("ll.head.next", *ll.head.next)
+
+	link, err = ll.getNode(node)
 
 	if err != nil {
 		fmt.Println("Error while fetching link: ", err)
